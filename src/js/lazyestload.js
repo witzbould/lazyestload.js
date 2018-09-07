39eaff80d90ccbb41698a0398c38af52684cd4cb
Tom Kopp <witzbould@live.com>
2018-09-07 19:24:55 +0200

    split into logical functions
    Closure Compiler simple - 343 bytes gzipped
    UglifyJS 3 - 351 bytes gzipped

--- src/js/lazyestload.js
+++ src/js/lazyestload.js
@@ -1,54 +1,66 @@
-;(function () {
+; (function () {
+
+    // img is a non-live dom-node
+    // ctx is a window object
+
+    const shouldLoad = (ctx, img) => {
+        const boundingRect = img.getBoundingClientRect();
+        const offset = 100;
+        const yPositionTop = boundingRect.top - ctx.innerHeight;
+        const yPositionBottom = boundingRect.bottom;
+
+        // if the top of the image is within 100px from the bottom of the viewport
+        // and if the bottom of the image is within 100px from the top of the viewport
+        // basically if the image is in the viewport, with a bit of buffer
+
+        return (yPositionTop <= offset && yPositionBottom >= -offset);
+    }
+
+    // img is a non-live dom-node
+
+    const load = (img) => {
+        let srcs;
+
+        // replace the src with the data-src
+
+        if (srcs = img.getAttribute("data-src")) {
+            img.src = srcs;
+        }
+
+        // replace the srcset with the data-srcset
+
+        if (srcs = img.getAttribute("data-srcset")) {
+            img.srcset = srcs;
+        }
+
+        // replace the source srcset's with the data-srcset's
+
+        if (img.parentElement.tagName === "PICTURE") {
+            img.parentElement
+                .querySelectorAll("source")
+                .forEach((el) => el.srcset = el.getAttribute("data-srcset"));
+        }
+
+        // wait until the new image is loaded
+
+        img.addEventListener('load', (event) => event.target.classList.remove("lazyestload"));
+    }
 
     // main function wrapper
+    // ctx is a window object
 
-    const lazyestload = () => {
+    const lazyestload = (ctx) => {
 
         // all the images with class lazyestload
 
-        const images = document.querySelectorAll("img.lazyestload");
-        let i = images.length;
-
-        // loop de loop
-
-        while (i--) {
-            let srcs;
-            const img = images[i];
-            const boundingRect = img.getBoundingClientRect();
-            const offset = 100;
-            const yPositionTop = boundingRect.top - window.innerHeight;
-            const yPositionBottom = boundingRect.bottom;
-
-            // if the top of the image is within 100px from the bottom of the viewport
-            // and if the bottom of the image is within 100px from the top of the viewport
-            // basically if the image is in the viewport, with a bit of buffer
-
-            if (yPositionTop <= offset && yPositionBottom >= -offset) {
-
-                // replace the src with the data-src
-
-                if (srcs = img.getAttribute("data-src")) {
-                    img.src = srcs;
+        document
+            .querySelectorAll("img.lazyestload")
+            .forEach((el) => {
+                if (shouldLoad(ctx, el)) {
+                    load(el);
                 }
+        });
 
-                // replace the srcset with the data-srcset
-
-                if (srcs = img.getAttribute("data-srcset")) {
-                    img.srcset = srcs;
-                }
-
-                // replace the source srcset's with the data-srcset's
-
-                if (img.parentElement.tagName === "PICTURE") {
-                    img.parentElement.querySelectorAll("source").forEach((el) => el.srcset = el.getAttribute("data-srcset"));
-                }
-
-                // wait until the new image is loaded
-
-                img.addEventListener('load', (event) => event.target.classList.remove("lazyestload"));
-
-            }
-        }
     }
 
     // run on debounced scroll event and once on load
@@ -59,8 +71,8 @@
 
     window.addEventListener("scroll", () => {
         clearTimeout(timeoutId);
-        timeoutId = setTimeout(lazyestload, 200);
+        timeoutId = setTimeout(lazyestload, 200, window);
     });
 
-    lazyestload();
+    lazyestload(window);
 })();
